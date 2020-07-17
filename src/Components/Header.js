6e95f327983b4f893f7ce00a266c1ec5289305ae
Sara Chaparro Diaz <sarachaparrodiaz@Saras-MacBook-Pro.local>
2020-07-17 11:48:55 -0700

    first personalized draft

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,9 +8,6 @@ class Header extends Component {
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
     }
 
     return (
@@ -34,12 +31,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">Hello! I'm {name}.</h1>
             <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
-            <hr />
-            <ul className="social">
-               {networks}
-            </ul>
          </div>
       </div>
 
